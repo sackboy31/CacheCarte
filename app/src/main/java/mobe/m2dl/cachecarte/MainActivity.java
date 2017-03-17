19010752ca9affd264fa5ace82bcbeb826659808
Sackboy31 <kevin.machu@hotmail.fr>
2017-03-17 08:48:21 +0100

    Initial commit

---
+++ app/src/main/java/mobe/m2dl/cachecarte/MainActivity.java
@@ -0,0 +1,13 @@
+package mobe.m2dl.cachecarte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
